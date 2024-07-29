5804178c945e2ff7aab6d79dd3523d8374eb7135
Java Tech Solutions <126051650+JavaaTechSolutions@users.noreply.github.com>
2024-07-29 14:57:52 +0530

    Added get all files api

--- src/main/java/com/jts/controller/FilesController.java
+++ src/main/java/com/jts/controller/FilesController.java
@@ -1,11 +1,15 @@
 package com.jts.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,18 +18,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.jts.entity.Files;
 import com.jts.service.FilesService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class FilesController {
+	
+	private static final Logger log = LoggerFactory.getLogger(FilesService.class);
 	
 	@Autowired
 	private FilesService filesService;
 	
 	@PostMapping("/uploadFilesIntoDB")
 	public ResponseEntity<String> storeFilesIntoDB(@RequestParam("file") MultipartFile file) throws IOException {
+		log.info("Enter into uploadFilesIntoDB method");
+		
 		String response = filesService.storeFile(file);
+		
+		log.info("Completed uploadFilesIntoDB method with response {}", response);
 		
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
@@ -35,6 +47,13 @@ public class FilesController {
 		byte[] imageData = filesService.getFiles(fileName);
 		
 		return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/png")).body(imageData);
+	}
+	
+	@GetMapping("/files")
+	public ResponseEntity<List<Files>> getAllFiles() {
+		List<Files> imageData = filesService.getAllFiles();
+		
+		return ResponseEntity.status(HttpStatus.OK).body(imageData);
 	}
 	
 	@PostMapping("/uploadFilesIntoFileSystem")
--- src/main/java/com/jts/service/FilesService.java
+++ src/main/java/com/jts/service/FilesService.java
@@ -2,7 +2,10 @@ package com.jts.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,58 +15,60 @@ import com.jts.entity.Files;
 
 @Service
 public class FilesService {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(FilesService.class);
+
 	@Autowired
 	private FileRepository fileRepository;
-	
+
 	private final String FILE_PATH = "E:\\projects\\images\\storage\\";
 
 	public String storeFile(MultipartFile file) throws IOException {
 		Files files = Files
-		.builder()
-		.name(file.getOriginalFilename())
-		.type(file.getContentType())
-		.imageData(file.getBytes())
-		.build();
-		
+				.builder()
+				.name(file.getOriginalFilename())
+				.type(file.getContentType())
+				.imageData(file.getBytes()).build();
+
 		files = fileRepository.save(files);
-		
+
+		log.info("File uploaded successfully into database with id {}", files.getId());
+
 		if (files.getId() != null) {
-			return "File uploaded successfuly into database";
+			return "File uploaded successfully into database";
 		}
-		
+
 		return null;
 	}
-	
+
 	public byte[] getFiles(String fileName) {
 		return fileRepository.findByName(fileName).getImageData();
 	}
 	
+	public List<Files> getAllFiles() {
+		return fileRepository.findAll();
+	}
+
 	public String storeDataIntoFileSystem(MultipartFile file) throws IOException {
 		String filePath = FILE_PATH + file.getOriginalFilename();
-		
-		Files files = Files
-		.builder()
-		.name(file.getOriginalFilename())
-		.path(filePath)
-		.type(file.getContentType())
-		.imageData(file.getBytes())
-		.build();
-		
+
+		Files files = Files.builder().name(file.getOriginalFilename()).path(filePath).type(file.getContentType())
+				.imageData(file.getBytes()).build();
+
 		files = fileRepository.save(files);
-		
+
 		file.transferTo(new File(filePath));
-		
+
 		if (files.getId() != null) {
 			return "File uploaded successfuly into database";
 		}
-		
+
 		return null;
 	}
-	
+
 	public byte[] downloadFilesFromFileSystem(String fileName) throws IOException {
 		String path = fileRepository.findByName(fileName).getPath();
-		
+
 		return java.nio.file.Files.readAllBytes(new File(path).toPath());
 	}
 }
